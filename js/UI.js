2fdfe868bca1656dc498536a4105cd81d53307a5
Mathias007 <mathias.najlepszy@gmail.com>
2021-01-14 17:57:24 +0100

    Add data attributes and update UI.js

--- js/UI.js
+++ js/UI.js
@@ -3,7 +3,14 @@ export class UI {
         board: "[data-board]",
         cell: "[data-cell]",
         counter: "[data-counter]",
-        timer: "[data-timer]"
+        timer: "[data-timer]",
+        resetButton: "[data-button-reset]",
+        easyButton: "[data-button-easy]",
+        normalButton: "[data-button-normal]",
+        resetButton: "[data-button-expert]",
+        modal: "[data-modal]",
+        modalHeader: "[data-modal-header]",
+        modalButton: "[data-modal-button]",
     };
 
     getElement(selector) {
